72381f3a6b13d2d4e2e9300545b551a30a3cc42d
liaozhuzhu <liao.zhu.cs@gmail.com>
2024-02-23 23:12:06 -0600

    Make urls / links clickable

--- client/src/app/(protected)/dashboard/page.tsx
+++ client/src/app/(protected)/dashboard/page.tsx
@@ -2,37 +2,47 @@
 import React, { useState, useEffect } from 'react';
 import { Grid } from '../../../types/grid';
 import { GridItem } from '../../../types/grid';
-import { ColDef } from 'ag-grid-community';
+import { ColDef, ICellRendererParams } from 'ag-grid-community';
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-quartz.css';
 import { AgGridReact } from 'ag-grid-react';
 
 export default function Profile() {
-    // usestate variable called items 
     const [items, setItems] = useState<GridItem[]>([]);
     const [headers, setHeaders] = useState<string[]>([]);
 
+    const setOfLinkSynonyms = new Set(['url', 'linkedin', 'github', 'portfolio', 'website', 'link', 'links', 'site']) 
+
     useEffect(() => {
         setItems([
-            { value: 'Liao', r: 0, c: 'feature1' },
+            { value: 'https://github.com/liaozhuzhu', r: 0, c: 'URL' },
             { value: 'Zhu', r: 0, c: 'feature2' },
-            { value: 'Goodbye', r: 1, c: 'feature1' },
+            { value: 'https://www.linkedin.com/in/liao-zhu/', r: 1, c: 'URL' },
             { value: 'Life', r: 1, c: 'feature2' },
         ]);
-        setHeaders(['feature1', 'feature2']);
+        setHeaders(['URL', 'feature2']);
     }, []);
+
+    function LinkComponent(props: ICellRendererParams) {
+        return (
+            <a href={props.value} target='_blank' className='underline'>
+                {props.value}
+            </a>
+        );
+    }
 
     // create a column definition for each header
     let colDefs: ColDef[] = headers.map(header => {
         return {
             field: header,
             flex: 1,
+            cellRenderer: setOfLinkSynonyms.has(header.toLowerCase()) ? LinkComponent : '',
         }
     });
 
     let grid: Grid = {
         headers: headers,
-        items: items
+        items: items,
     };
 
     let rowData: any[] = [];
@@ -45,7 +55,7 @@ export default function Profile() {
     return (
         <div className='flex flex-col'>
             <div className='flex flex-col h-screen w-full justify-center'>
-                <div className='ag-theme-quartz-dark' style={{ height: '100%', width: '100%' }}>
+                <div className='ag-theme-quartz' style={{ height: '100%', width: '100%' }}>
                     <AgGridReact
                         rowData={rowData}
                         columnDefs={colDefs}
