3a8208e5185ac2febfc4ecb15fdb164dec8020e0
liaozhuzhu <liao.zhu.cs@gmail.com>
2024-02-24 12:48:28 -0600

    fix merge conflict?

--- client/src/components/LandingMain.tsx
+++ client/src/components/LandingMain.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React from 'react';
-import { Inconsolata } from '@next/font/google';
+// import { Inconsolata } from '@next/font/google';
 
 const LandingMain = () => {
   return (
@@ -8,8 +8,8 @@ const LandingMain = () => {
       <h1 className=" text-4xl font-bold text-center mb-4" style={{ fontSize: '4.7rem', lineHeight: '1'}}>
         Cut the grunt work out of lead generation
       </h1>
-      <p className="text-center text-center mx-auto max-w-[52rem] text-white60" style={{fontFamily: 'Inconsolata, "Times New Roman"', lineHeight: '1.5'}}>
-        95% of lead generation is spent manually going through websites looking. Lead2B automates that whole process
+      <p className="text-center text-center mx-auto max-w-[52rem]" style={{font: 'Inconsolata, Times New Roman', lineHeight: '1.5'}}>
+        95% of lead generation is spent manually going through websites looking. sheetz automates that whole process
       </p>
     </div>
   );
