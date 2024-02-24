d295687f8ba01cdec861a7e2cd7f172e96a4616c
liaozhuzhu <liao.zhu.cs@gmail.com>
2024-02-24 09:50:35 -0600

    Profile on navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -21,16 +21,22 @@ const Navbar: React.FC<NavbarProps> = ({ sheets }: NavbarProps) => { // sheet is
     return (
         <div className='flex justify-between bg-brandColor p-2 min-w-[250px]'>
             <div className='flex flex-col justify-start items-center w-full p-3'>
-                <div className='text-white text-md cursor-pointer p-2 rounded-md flex items-center justify-between w-full mb-16 bg-brandColor hover:bg-[#6500F0] transition'>
+                <div className='text-white text-md cursor-pointer p-2 rounded-md flex items-center justify-between w-full mb-16 bg-brandColor hover:bg-brandHoverColor transition'>
                     <p>Create Gridsheet</p>
                     <FontAwesomeIcon icon={faSquarePlus} className="text-md"/>
                 </div>
-                <h3 className="w-full text-md text-darkColor">Your Gridsheets</h3>
-                <ul className='flex flex-col justify-center items-start text-left text-[15px] w-full'>
-                    {userSheets.map((sheet) => (
-                        <NavbarItem key={sheet.id} sheetName={sheet.name} />
-                    ))}
-                </ul>
+                <div className="flex justify-center items-center pl-2 flex-col w-full">
+                    <h3 className="w-full text-md text-darkColor">Your Gridsheets</h3>
+                    <ul className='flex flex-col justify-center items-start text-left w-full'>
+                        {userSheets.map((sheet) => (
+                            <NavbarItem key={sheet.id} sheetName={sheet.name} />
+                        ))}
+                    </ul>
+                </div>
+                <div className='mt-auto flex justify-start gap-2 items-center text-white text-md cursor-pointer p-2 rounded-md w-full bg-brandColor hover:bg-brandHoverColor transition'>
+                    <img src="https://via.placeholder.com/150" alt="profile" className="rounded-full h-9 w-9"/>
+                    <p>User Profile</p>
+                </div>
             </div>
         </div>
     )
