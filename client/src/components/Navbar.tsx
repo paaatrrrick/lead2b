730cbbb16ef757101151f04fac517fa1588a1702
liaozhuzhu <liao.zhu.cs@gmail.com>
2024-02-24 12:01:25 -0600

    Added logout to nav

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -4,10 +4,11 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSquarePlus } from '@fortawesome/free-solid-svg-icons';
 import NavbarItem from './NavbarItem';
 import { Sheet } from '../types/sheet';
+import UserSettings from './UserSettings'
 
 export default function Navbar({ sheets, setIsNewSheet } : { sheets: Sheet[], setIsNewSheet(isNewSheet: boolean): void }){ // sheet is { name: string, id: number }
 
-    // create a usestate variable called userSheets that's a list of sheets
+    const [userSettingsShowing, setUserSettingsShowing] = useState<boolean>(false); 
     const [userSheets, setUserSheets] = useState<Sheet[]>([]);
 
     useEffect(() => {
@@ -17,21 +18,22 @@ export default function Navbar({ sheets, setIsNewSheet } : { sheets: Sheet[], se
     return (
         <div className='flex justify-between bg-brandColor p-2 min-w-[250px]'>
             <div className='flex flex-col justify-start items-center w-full p-3'>
-                <div onClick={() => setIsNewSheet(true)} className='text-white text-md cursor-pointer p-2 rounded-md flex items-center justify-between w-full mb-16 bg-brandColor hover:bg-brandHoverColor transition'>
-                    <p>Create Gridsheet</p>
-                    <FontAwesomeIcon icon={faSquarePlus} className="text-md"/>
+                <div onClick={() => setIsNewSheet(true)} className='text-white text-md cursor-pointer p-2 rounded-md flex items-center justify-start gap-2 text-md w-full mb-16 bg-brandColor hover:bg-brandHoverColor transition'>
+                    <p>Create Sheet</p>
+                    <FontAwesomeIcon icon={faSquarePlus}/>
                 </div>
-                <div className="flex justify-center items-center pl-2 flex-col w-full">
-                    <h3 className="w-full text-md text-darkColor">Your Gridsheets</h3>
+                <div className="flex justify-center items-center flex-col w-full">
+                    <h3 className="w-full text-md text-zinc-500 p-2 pb-0">Your Sheets</h3>
                     <ul className='flex flex-col justify-center items-start text-left w-full'>
                         {userSheets.map((sheet) => (
                             <NavbarItem key={sheet.id} sheetName={sheet.name} handleClick={() => setIsNewSheet(false)}/>
                         ))}
                     </ul>
                 </div>
-                <div className='mt-auto flex justify-start gap-2 items-center text-white text-md cursor-pointer p-2 rounded-md w-full bg-brandColor hover:bg-brandHoverColor transition'>
+                <div className='relative mt-auto flex justify-start gap-2 items-center text-white text-md cursor-pointer p-2 rounded-md duration-450 z-0 w-full bg-brandColor hover:bg-brandHoverColor transition' onClick={() => setUserSettingsShowing(!userSettingsShowing)}>
                     <img src="https://via.placeholder.com/150" alt="profile" className="rounded-full h-9 w-9"/>
                     <p>User Profile</p>
+                    {userSettingsShowing && <UserSettings/>}
                 </div>
             </div>
         </div>
--- client/src/components/NavbarItem.tsx
+++ client/src/components/NavbarItem.tsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 
 export default function NavbarItem({ sheetName, handleClick }: { sheetName: string, handleClick(): void}) {
     return (
-        <li onClick={handleClick} className='flex justify-center items-center text-white p-2 pl-0 text-md cursor-pointer rounded-md'>
+        <li onClick={handleClick} className='flex justify-start items-center text-white p-2 text-md cursor-pointer rounded-md duration-450 z-0 hover:bg-brandHoverColor transition w-full'>
             {sheetName}
         </li>
     )
